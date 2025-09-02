9341e3d578a741bf94b1ba8f455ae4cde3649384
Dogan Alici <doganalici2144@gmail.com>
2025-09-02 08:31:16 +0300

    Add project files.

---
+++ Day01_VariablesAndTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01_VariablesAndTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yas = 27;
+            double maas = 32500.50;
+            string ad = "Doğan";
+            bool yazilimciMi = true;
+            string enSevdigiYemek = "Patlican";
+            double boy = 1.92;
+            bool kodlamayiSeviyorMu = true;
+
+            Console.WriteLine($"Ad : {ad}\nYaş : {yas}\nMaaş : {maas}\nYazılımcı mı? : {yazilimciMi}\nEn sevdiği yemek : {enSevdigiYemek}\nBoy : {boy}\nKodlamayı seviyor mu? : {kodlamayiSeviyorMu}\n\n\n ");
+
+            Console.Write("Adınızı yazınız : ");
+            string kullaniciAdi = Console.ReadLine();
+
+            Console.Write("Yaşınızı giriniz : ");
+            int kullaniciYas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\n\nKullanıcının adı : {kullaniciAdi}\nKullanıcının yaşı : {kullaniciYas}");
+        }
+    }
+}
