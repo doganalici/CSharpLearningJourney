d545dd95db6dd6cb9eab95851ce8334747492358
Dogan Alici <doganalici2144@gmail.com>
2025-09-16 07:56:50 +0300

    Day 9 Update

---
+++ Day09_ArraysAndMethods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day09_ArraysAndMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 5, 8, 12, 3, 7 };
+            DiziYazdir(sayilar);
+            Console.WriteLine("\n*******************************\n");
+            int toplam = DiziToplam(sayilar);
+            Console.WriteLine($"Dizinin toplamı : {toplam}");
+            Console.WriteLine("\n**************************\n");
+            double ortalama = DiziOrtalama(sayilar);
+            Console.WriteLine($"Dizinin ortalaması : {ortalama}");
+            Console.WriteLine("\n*******************************\n");
+
+            UseArray useArray = new UseArray();
+            useArray.DiziAl();
+            Console.WriteLine("\n*******************************\n");
+
+        }
+
+        public static void DiziYazdir(int[] dizi)
+        {
+            Console.WriteLine("Dizinin elemanları;");
+            foreach (int i in dizi)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public static int DiziToplam(int[] dizi)
+        {
+            int toplam = 0;
+            foreach (int i in dizi)
+            {
+                toplam += i;
+            }
+            return toplam;
+        }
+
+        public static double DiziOrtalama(int[] dizi)
+        {
+            return (double)DiziToplam(dizi) / dizi.Length;
+        }
+
+    }
+}
---
+++ Day09_ArraysAndMethods/UseArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day09_ArraysAndMethods
+{
+    public class UseArray
+    {
+        public void DiziAl()
+        {
+            Console.Write("Kaç adet sayı gireceksiniz : ");
+            int adet = Convert.ToInt32(Console.ReadLine());
+            int[] sayilar = new int[adet];
+
+            for (int i = 0; i < adet; i++)
+            {
+                Console.Write($"{i + 1}.sayı : ");
+                sayilar[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine($"\nDizideki en büyük sayı : {MaxBul(sayilar)}");
+            Console.WriteLine($"Dizideki en küçük sayı : {MinBul(sayilar)}");
+        }
+        public static int MaxBul(int[] dizi)
+        {
+            int max = dizi[0];
+            foreach (int i in dizi)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
+        public static int MinBul(int[] dizi)
+        {
+            int min = dizi[0];
+            foreach (int i in dizi)
+            {
+                if (i < min)
+                {
+                    min = i;
+                }
+            }
+            return min;
+        }
+    }
+}
