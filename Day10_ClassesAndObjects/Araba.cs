c96ee1aac4c6337f5c4ec4b0e0d4d1f912cc7568
Dogan Alici <doganalici2144@gmail.com>
2025-09-17 06:37:10 +0300

    Day 10 Update

---
+++ Day10_ClassesAndObjects/Araba.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_ClassesAndObjects
+{
+    public class Araba
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Yil { get; set; }
+        public string Renk { get; set; }
+
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($"Marka : {Marka}\n" +
+                $"Model : {Model}\n" +
+                $"Yıl : {Yil}\n" +
+                $"Renk : {Renk}");
+        }
+        public int YasiHesapla()
+        {
+            int yil=DateTime.Now.Year;
+            return yil-Yil;
+        }
+    }
+}
---
+++ Day10_ClassesAndObjects/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_ClassesAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba araba1 = new Araba();
+            araba1.Marka = "Toyota";
+            araba1.Model = "Corolla";
+            araba1.Yil = 2020;
+            araba1.Renk = "Beyaz";
+
+
+            Araba araba2 = new Araba();
+            araba2.Marka = "BMW";
+            araba2.Model = "320i";
+            araba2.Yil = 2022;
+            araba2.Renk = "Siyah";
+
+            araba1.BilgileriYazdir();
+            Console.WriteLine($"Arabanın yaşı : {araba1.YasiHesapla()}");
+            Console.WriteLine("\n*********************\n");
+            araba2.BilgileriYazdir();
+            Console.WriteLine($"Arabanın yaşı : {araba2.YasiHesapla()}");
+        }
+    }
+}
