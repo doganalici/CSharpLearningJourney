7b5bd2ca16df286e8e01a2342f90d529572a063f
Dogan Alici <doganalici2144@gmail.com>
2025-09-18 06:39:07 +0300

    Day 11 Update

---
+++ Day11_Constructors/Araba.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_Constructors
+{
+    public class Araba
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Yil { get; set; }
+        public string Renk { get; set; }
+
+
+        //1-Parametresiz Constructor (varsayılan değerler)
+        public Araba()
+        {
+            Marka = "Bilinmiyor";
+            Model = "Bilinmiyor";
+            Yil = 0;
+            Renk = "Bilinmiyor";
+        }
+
+        //2-Parametreli Constructor
+        public Araba(string marka, string model, int yil, string renk)
+        {
+            Marka = marka;
+            Model = model;
+            Yil = yil;
+            Renk = renk;
+        }
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($"Marka : {Marka}\n" +
+                $"Model : {Model}\n" +
+                $"Yıl : {Yil}\n" +
+                $"Renk : {Renk}");
+        }
+    }
+}
---
+++ Day11_Constructors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba araba1 = new Araba();
+            Araba araba2 = new Araba();
+            araba2.Marka = "Volkswagen";
+            araba2.Model = "Passat";
+            araba2.Yil = 2011;
+            araba2.Renk = "Siyah";
+
+            Araba araba3 = new Araba("BMW", "320i", 2022, "Beyaz");
+
+            Console.WriteLine("1.Arabanın Özellikleri\n");
+            araba1.BilgileriYazdir();
+            Console.WriteLine("\n***************************\n");
+            Console.WriteLine("2.Arabanın Özellikleri\n");
+            araba2.BilgileriYazdir();
+            Console.WriteLine("\n***************************\n");
+            Console.WriteLine("3.Arabanın Özellikleri\n");
+            araba3.BilgileriYazdir();
+
+
+        }
+    }
+}
