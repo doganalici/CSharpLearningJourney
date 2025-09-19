33563a109278df4604a01c76e6619d105bf1c680
Dogan Alici <doganalici2144@gmail.com>
2025-09-19 06:33:57 +0300

    Day 12 Update

---
+++ Day12_Encapsulation/Ogrenci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12_Encapsulation
+{
+    public class Ogrenci
+    {
+        private int yas;
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Yas
+        {
+            get { return yas; }
+            set
+            {
+                if (value < 0 || value > 110)
+                {
+                    Console.WriteLine("Geçersiz yaş girildi!");
+                }
+                else
+                {
+                    yas = value;
+                }
+            }
+        }
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($"Ad : {Ad}\n" +
+                $"Soyad : {Soyad}\n" +
+                $"Yaş : {yas}");
+        }
+    }
+}
---
+++ Day12_Encapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci1 = new Ogrenci();
+            ogrenci1.Ad = "Ahmet";
+            ogrenci1.Soyad = "Yılmaz";
+            ogrenci1.Yas = 25;
+            ogrenci1.BilgileriYazdir();
+            Console.WriteLine("\n------------------------------\n");
+
+            Ogrenci ogrenci2 = new Ogrenci() { Ad = "Mehmet", Soyad = "Demir", Yas = -5 };
+
+            ogrenci2.BilgileriYazdir();
+
+        }
+    }
+}
