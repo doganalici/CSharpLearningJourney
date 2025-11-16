09658a157c62f1e53222791d8bf316407a77c87b
Dogan Alici <doganalici2144@gmail.com>
2025-11-16 12:07:29 +0300

    Day 19 Update

---
+++ Day19_QueueStructure/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day19_QueueStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> musteriler = new Queue<string>();
+
+            musteriler.Enqueue("Ahmet");
+            musteriler.Enqueue("Mehmet");
+            musteriler.Enqueue("Ayşe");
+            musteriler.Enqueue("Fatma");
+
+            Console.WriteLine("Müşteri kuyruğu : ");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri);
+            }
+
+            Console.WriteLine($"\nSonraki müşteriş : {musteriler.Peek()}");
+
+            Console.WriteLine($"\n{musteriler.Dequeue()} isimli müşterinin işlemi tamamlandı");
+
+            Console.WriteLine("\nGüncel Kuyruk : ");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri);
+            }
+        }
+    }
+}
