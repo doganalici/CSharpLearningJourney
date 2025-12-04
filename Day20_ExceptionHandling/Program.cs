5c878ea34ff93d9f6b7ea0754aaacbe87aa5abd6
Dogan Alici <doganalici2144@gmail.com>
2025-12-04 20:13:56 +0300

    Day 20 Update

---
+++ Day20_ExceptionHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20_ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = int.Parse("abc");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Hata oluştu : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Her durumda çalışan blok.");
+            }
+        }
+    }
+}
