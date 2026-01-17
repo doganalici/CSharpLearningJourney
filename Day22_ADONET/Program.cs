dcf24cb508d32d1adb57f5f78d9226f190e455b4
Dogan Alici <doganalici2144@gmail.com>
2026-01-18 02:38:54 +0300

    Day 22 Update

---
+++ Day22_ADONET/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22_ADONET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=DESKTOP-1BH7EFE\\MSSQLSERVER01;Database=TestDb;Trusted_Connection=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Veritabanı bağlantısı başarılı");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bağlantı hatası X");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine("Bağlantı kapatıldı.");
+            }
+        }
+    }
+}
