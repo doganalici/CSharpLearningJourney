936b240ffb7d48be07c467aa35fd917be591f579
Dogan Alici <doganalici2144@gmail.com>
2026-01-31 11:46:07 +0300

    Day 24 Update

---
+++ Day24_DataInsertADO.NET/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day24_DataInsertADO.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+                "Server=DESKTOP-1BH7EFE\\MSSQLSERVER01;Database=TestDb;Trusted_Connection=True";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query =
+                        "INSERT INTO Ogrenciler (Ad, Soyad) VALUES (@ad, @soyad)";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    command.Parameters.AddWithValue("@ad", "Ali");
+                    command.Parameters.AddWithValue("@soyad", "Kaya");
+
+                    int sonuc = command.ExecuteNonQuery();
+
+                    if (sonuc > 0)
+                        Console.WriteLine("Kayıt başarıyla eklendi.");
+                    else
+                        Console.WriteLine("Kayıt eklenemedi.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+        }
+    }
+}
