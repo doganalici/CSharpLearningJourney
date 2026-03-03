0191b7dd637ad3e49c7cb1326b87819f2776e8ee
Dogan Alici <doganalici2144@gmail.com>
2026-03-03 12:48:21 +0300

    Day 28 Update

---
+++ Day28_ModelClassMapping/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Day28_ModelClassMapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=DESKTOP-1BH7EFE\\MSSQLSERVER01;Database=TestDb;Trusted_Connection=True";
+
+            List<Ogrenci> ogrenciler = new List<Ogrenci>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT Id,Ad,Soyad FROM Ogrenciler";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        int id = (int)reader["Id"];
+                        string ad = reader["Ad"].ToString();
+                        string soyad = reader["Soyad"].ToString();
+
+                        Ogrenci ogrenci = new Ogrenci(id, ad, soyad);
+                        ogrenciler.Add(ogrenci);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Hatalı tuşlama yaptınız : {ex.Message}");
+                }
+            }
+            foreach (var ogrenci in ogrenciler)
+            {
+                ogrenci.BilgileriYazdir();
+            }
+        }
+    }
+}
