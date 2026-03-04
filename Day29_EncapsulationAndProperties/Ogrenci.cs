de34b163b1a300bb1ede8ccaafabfa99746b39c4
Dogan Alici <doganalici2144@gmail.com>
2026-03-04 06:00:35 +0300

    Day 29 Update

---
+++ Day29_EncapsulationAndProperties/Ogrenci.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day29_EncapsulationAndProperties
+{
+    public class Ogrenci
+    {
+        private string _ad;
+        private string _soyad;
+        private int _yas;
+
+        public int Id { get; set; }
+        public string Ad
+        {
+            get { return _ad; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _ad = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ad boş olamaz");
+                }
+            }
+        }
+        public string Soyad
+        {
+            get { return _soyad; }
+            set { _soyad = value; }
+        }
+
+        public int Yas
+        {
+            get { return _yas; }
+            set
+            {
+                if (value > 0)
+                {
+                    _yas = value;
+                }
+                else
+                {
+                    Console.WriteLine("Yaş sıfırdan büyük olmalıdır");
+                }
+            }
+        }
+
+        public string TamAd
+        {
+            get { return $"{Ad} {Soyad}"; }
+        }
+
+        public Ogrenci(int id,string ad,string soyad,int yas)
+        {
+            Id=id;
+            Ad = ad;
+            Soyad = soyad;
+            Yas = yas;
+        }
+
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($"{Id} - {TamAd} - {Yas}");
+        }
+    }
+}
---
+++ Day29_EncapsulationAndProperties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day29_EncapsulationAndProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci1 = new Ogrenci(1,"Ahmet","Yılmaz",20);
+            ogrenci1.BilgileriYazdir();
+
+            Console.WriteLine("---- Hatalı veri test ----");
+            Ogrenci ogrenci2 = new Ogrenci(2,"","Kara",-5);
+            ogrenci2.BilgileriYazdir();
+
+        }
+    }
+}
