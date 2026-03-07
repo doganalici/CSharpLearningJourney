2f6449fb1ffeb3c1859880021ddb8999d5571032
Dogan Alici <doganalici2144@gmail.com>
2026-03-07 06:45:32 +0300

    Day 35 Update

---
+++ Day35_Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day35_Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Islem islem;
+
+            islem = Topla;
+            Console.WriteLine(islem(3, 4));
+
+            islem = Carp;
+            Console.WriteLine(islem(3, 4));
+        }
+
+        static int Topla(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Carp(int a, int b)
+        {
+            return a * b;
+        }
+    }
+    public delegate int Islem(int a, int b);
+
+}
