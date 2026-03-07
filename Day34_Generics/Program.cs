00609652cfb8513e1108e13b3f2be905317fbfce
Dogan Alici <doganalici2144@gmail.com>
2026-03-07 06:24:36 +0300

    Day 33 Update

---
+++ Day34_Generics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day34_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kutu<int> sayiKutusu = new Kutu<int>();
+            sayiKutusu.Deger = 10;
+
+            Kutu<string> yaziKutusu = new Kutu<string>();
+            yaziKutusu.Deger = "Merhaba";
+
+            Console.WriteLine(sayiKutusu.Deger);
+            Console.WriteLine(yaziKutusu.Deger);
+        }
+    }
+}
