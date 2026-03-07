4e67939fea7be7ee1df96be0dd7e38aeb0fdad99
Dogan Alici <doganalici2144@gmail.com>
2026-03-07 07:06:40 +0300

    Day 36 Update

---
+++ Day36_LambdaExpressions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day36_LambdaExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int, int, int> topla = (a, b) => a + b;
+
+            Console.WriteLine(topla(3, 4));
+
+
+
+            Action<string> yazdir = mesaj => Console.WriteLine(mesaj);
+
+            yazdir("Merhaba Lambda");
+
+
+            Predicate<int> ciftMi = sayi => sayi % 2 == 0;
+
+            Console.WriteLine(ciftMi(10));
+
+
+            List<int> sayilar = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+            var ciftler = sayilar.Where(x => x % 2 == 0);
+
+            foreach (var sayi in ciftler)
+            {
+                Console.WriteLine(sayi);
+
+
+            }
+        }
+    }
+}
